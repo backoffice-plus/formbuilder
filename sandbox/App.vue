b77019e2d719affd112a5f6e20bc3ba68989c071
Julian Haupt <julian@haupt.media>
2023-01-08 16:03:45 +0100

    updated vuedraggable

--- sandbox/App.vue
+++ sandbox/App.vue
@@ -1,11 +1,12 @@
 <template>
-  <main>
     <FormBuilder/>
-  </main>
 </template>
 
 <script setup lang="ts">
 import {FormBuilder} from "../src/index.ts";
 </script>
 
-<style lang="scss" scoped></style>
+
+<style scoped>
+
+</style>
