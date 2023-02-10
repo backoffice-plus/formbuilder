94798d5f9a22498f592618aae4cd41d335ebae69
David Wolter <david@dampfer.net>
2023-02-10 14:33:42 +0100

    remove files, fix import

--- src/index.ts
+++ src/index.ts
@@ -42,4 +42,4 @@ export const jsonFormRenderes = Object.freeze([
 export {emitter} from './lib/mitt'
 
 
-export {default as Vuedraggable} from "../packages/_vuedraggable/";
+export {default as Vuedraggable} from "../packages/_vuedraggable/src/vuedraggable.js";
