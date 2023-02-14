aef70ab3568f9e915e93ed98b214ec855d9b7a8f
David Wolter <david@dampfer.net>
2023-02-14 12:11:41 +0100

    use single file component for renderer

--- packages/boplus-vue-vanilla/src/index.ts
+++ packages/boplus-vue-vanilla/src/index.ts
@@ -1,4 +1,4 @@
-import {entry as tabsCategorizationRenderer} from "./components/TabsCategorizationRenderer.vue";
+import TabsCategorizationRenderer from "./components/TabsCategorizationRenderer.component.vue";
 import {entry as oneOfRenderer} from "./components/OneOfRenderer.vue";
 import {entry as allOfRenderera} from "./components/AllOfRenderer.vue";
 import {entry as anyOfRenderer} from "./components/AnyOfRenderer.vue";
@@ -6,8 +6,17 @@ import {entry as objectRenderer} from "./components/ObjectRenderer.vue";
 import {entry as arrayControlRenderer} from "./components/ArrayControlRenderer.vue";
 import {entry as enumArrayRenderer} from "./components/EnumArrayRenderer.vue";
 
+import {and, categorizationHasCategory, rankWith} from "@jsonforms/core";
+import type {JsonFormsRendererRegistryEntry} from "@jsonforms/core";
+import {uiTypeIs} from "@jsonforms/core/src/testers/testers";
+
+export const tabsCategorizationRendererEntry = {
+    renderer: TabsCategorizationRenderer,
+    tester: rankWith(2, and(uiTypeIs('Categorization'), categorizationHasCategory))
+} as JsonFormsRendererRegistryEntry;
+
 export const boplusVueVanillaRenderers = [
-    tabsCategorizationRenderer,
+    tabsCategorizationRendererEntry,
     oneOfRenderer,
     allOfRenderera,
     anyOfRenderer,
