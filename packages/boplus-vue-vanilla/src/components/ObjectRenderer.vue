51fa3f6f6dd2e83a27f5273017a04bbadd09da79
David Wolter <david@dampfer.net>
2023-02-16 15:17:57 +0100

    fix visible check

--- packages/boplus-vue-vanilla/src/components/ObjectRenderer.vue
+++ packages/boplus-vue-vanilla/src/components/ObjectRenderer.vue
@@ -31,41 +31,41 @@ import {cloneDeep, isEmpty, isObject} from "lodash";
 const props = defineProps(rendererProps<ControlElement>());
 
 const input = useVanillaControl(useJsonFormsControlWithDetail(props)) as any
+const control = input.control;
 
-const control = input.control.value as any;
 const nested = useNested('object');
 
 const hasAdditionalProperties = computed((): boolean => {
   return (
-      !isEmpty(control.schema.patternProperties) ||
-      isObject(control.schema.additionalProperties)
+      !isEmpty(control.value.schema.patternProperties) ||
+      isObject(control.value.schema.additionalProperties)
       // do not support - additionalProperties === true - since then the type should be any and we won't know what kind of renderer we should use for new properties
   );
 });
 
 const showAdditionalProperties = computed((): boolean => {
-  const showAdditionalProperties = control.uischema.options?.showAdditionalProperties;
+  const showAdditionalProperties = control.value.uischema.options?.showAdditionalProperties;
   return showAdditionalProperties === undefined || showAdditionalProperties === true;
 });
 
 const detailUiSchema = computed((): UISchemaElement => {
   const uiSchemaGenerator = () => {
-    const uiSchema = Generate.uiSchema(control.schema, 'Group');
-    if (isEmpty(control.path)) {
+    const uiSchema = Generate.uiSchema(control.value.schema, 'Group');
+    if (isEmpty(control.value.path)) {
       uiSchema.type = 'VerticalLayout';
     } else {
-      (uiSchema as GroupLayout).label = control.label;
+      (uiSchema as GroupLayout).label = control.value.label;
     }
     return uiSchema;
   };
   let result = findUISchema(
-      control.uischemas,
-      control.schema,
-      control.uischema.scope,
-      control.path,
+      control.value.uischemas,
+      control.value.schema,
+      control.value.uischema.scope,
+      control.value.path,
       uiSchemaGenerator,
-      control.uischema,
-      control.rootSchema
+      control.value.uischema,
+      control.value.rootSchema
   );
   if (nested.level > 0) {
     result = cloneDeep(result);
