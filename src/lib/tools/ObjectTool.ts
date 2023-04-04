1dafe7661ecaf2133ba6975b44cf49d31ca2b52b
David Wolter <david@dampfer.net>
2023-04-04 13:04:52 +0200

    add rules & styles to object

--- src/lib/tools/ObjectTool.ts
+++ src/lib/tools/ObjectTool.ts
@@ -30,15 +30,20 @@ export class ObjectTool extends AbstractTool implements ToolInterface {
             propertyName: this.propertyName,
             type: this.schema.type,
             schema: schema,
+            ...subschemas.prepareOptionDataRule(context, this.schema, this.uischema),
+            ...subschemas.prepareOptionDataStyles(context, this.schema, this.uischema),
             ...subschemas.prepareOptionDataValidation(context, this.schema, this.uischema),
             ...subschemas.prepareOptionDataconditional(context, this.schema, this.uischema),
             ...subschemas.prepareOptionDataDefinitions(context, this.schema, this.uischema),
+            _isUischema:'uischema' === context?.builder
         } as any;
     }
 
     optionDataUpdate(context: ToolContext, data: Record<string, any>): void {
         updatePropertyNameAndScope(data?.propertyName, this)
 
+        subschemas.setOptionDataRule(this.schema, this.uischema, data);
+        subschemas.setOptionDataStyles(this.schema, this.uischema, data);
         subschemas.setOptionDataValidation(this.schema, this.uischema, data);
         subschemas.setOptionDataconditional(this.schema, this.uischema, data);
         subschemas.setOptionDataDefinitions(this.schema, this.uischema, data);
