8d8492db1ca3036cbaf439484d12bfbb463d8dae
David Wolter <david@dampfer.net>
2023-05-23 15:11:23 +0200

    create DisclosureRenderer

---
+++ src/components/renderers/DisclosureRenderer.vue
@@ -0,0 +1,89 @@
+<template>
+
+    <details>
+        <summary><span>{{ layout.label }}</span></summary>
+
+        <dispatch-renderer
+            v-for="(element, index) in layout.uischema.elements"
+                :schema="layout.schema"
+                :uischema="element"
+                :path="layout.path"
+                :enabled="layout.enabled"
+                :renderers="layout.renderers"
+                :cells="layout.cells"
+        />
+
+    </details>
+
+</template>
+
+
+<style scoped>
+details {
+    min-height:20px;
+    margin-top:13px;
+
+    @apply border border-gray-300
+
+    relative
+}
+details summary {
+    top: -13px;
+
+    @apply px-2 m-0
+
+     left-2 right-2
+
+     absolute
+
+        bg-white
+
+    cursor-pointer
+}
+details summary span {
+    @apply px-2
+}
+details summary:hover span {
+    @apply bg-gray-300
+}
+details[open] {
+    @apply p-1
+}
+
+details[open] summary {
+    /*@apply p-0*/
+}
+
+</style>
+
+
+<script lang="ts">
+import {defineComponent} from "vue";
+import type {JsonFormsRendererRegistryEntry, Layout} from '@jsonforms/core';
+import {rankWith, uiTypeIs} from "@jsonforms/core";
+import type {RendererProps} from '@jsonforms/vue';
+import {DispatchRenderer, rendererProps, useJsonFormsLayout,} from '@jsonforms/vue';
+
+const disclosureRenderer = defineComponent({
+    name: 'disclosure-renderer',
+    components: {
+        DispatchRenderer,
+    },
+    props: {
+        ...rendererProps<Layout>(),
+    },
+    setup(props: RendererProps<Layout>) {
+        return {
+            ...useJsonFormsLayout(props),
+        };
+    }
+});
+
+export default disclosureRenderer;
+
+export const entry: JsonFormsRendererRegistryEntry = {
+    renderer: disclosureRenderer,
+    tester: rankWith(2, uiTypeIs('Disclosure')),
+};
+
+</script>
