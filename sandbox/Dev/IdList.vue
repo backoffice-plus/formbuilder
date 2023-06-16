61f00739498a5632b389a15b844660cccfe3cfdf
David Wolter <david@dampfer.net>
2023-06-16 13:09:40 +0200

    dev

--- sandbox/Dev/IdList.vue
+++ sandbox/Dev/IdList.vue
@@ -1,6 +1,15 @@
 <template>
 
-    <pre class="text-xs"><span :style="'background-color:#'+getId(tool.uuid)">{{ getId(tool.uuid) }}</span> {{ tool.constructor.name }}</pre>
+    <pre class="text-xs whitespace-nowrap">
+
+        <span :style="'background-color:#'+getId(tool.uuid)" title="tool id"> {{ getId(tool.uuid) }}</span>
+
+        <dfn :title="tool.propertyName">{{ tool.constructor.name }}</dfn>
+
+        <span :style="'background-color:#'+getId(tool.edge.schemaParent?.uuid)" title="schema parent id">{{ getId(tool.edge.schemaParent?.uuid) ?? "[]" }}</span>
+        <span :style="'background-color:#'+getId(tool.edge.uiParent?.uuid)" title="ui parent id">{{ getId(tool.edge.uiParent?.uuid) ?? "[]" }}</span>
+
+    </pre>
     <ul v-for="(child,k,i) in childs" v-if="childs" class="ml-4 font-mono">
         <li class="list-disc">
             <IdList :tool="child" v-if="child.uuid"/>
@@ -14,6 +23,12 @@ pre span {
     color:white;;
     text-shadow: 1px 1px #000;
 }
+dfn, span {
+    @apply
+    cursor-help
+}dfn {
+     color:unset
+ }
 </style>
 
 <script setup>
