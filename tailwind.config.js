dff263344a64992e6347b4ab0abdbf12f9cbac72
David Wolter <david@dampfer.net>
2024-01-29 13:39:13 +0100

    remove unused tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,26 +6,5 @@ module.exports = {
   content: [
     "./src/components/**/*.{vue,js,ts,jsx,tsx}",
     "./sandbox/**/*.{vue,js,ts,jsx,tsx}",
-  ],
-  theme: {
-    colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      black: colors.black,
-      white: colors.white,
-      gray: colors.gray,
-
-      red: colors.red,
-      green: colors.green,
-      yellow: colors.yellow,
-      blue: colors.blue,
-
-      'base-100': 'var(--base-100)',
-      'base-200': 'var(--base-200)',
-      'base-300': 'var(--base-300)',
-      'primary': 'var(--primary)',
-    },
-    extend: {},
-  },
-  plugins: [],
+  ]
 }
